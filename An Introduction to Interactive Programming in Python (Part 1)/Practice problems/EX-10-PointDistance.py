60d19263ecf541266cec0d3e1986e0efe739a580
monishappusamy <monishappusamy@gmail.com>
2015-05-24 23:55:20 +0530

    Execsie no.10

---
+++ An Introduction to Interactive Programming in Python (Part 1)/Practice problems/EX-10-PointDistance.py
@@ -0,0 +1,16 @@
+# Compute the distance between the points (x0, y0) and (x1, y1).
+
+###################################################
+# Distance formula
+# Student should enter statement on the next line.
+
+# Python statement that calculates and prints the distance between the points (2,2) and (5,6).
+
+print ((2-5)**2 + (2-6)**2) ** (1.0/2.0)
+
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#5.0
